7945387d966f04e45a77c4704cb4e2ec8a6e425f
victordelval <victor.val.andres@gmail.com>
2018-12-02 16:50:37 +0100

    Add handlers to create and get user/s

--- handler.js
+++ handler.js
@@ -1,14 +1,91 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+const AWS = require('aws-sdk');
+const bodyParser = require('body-parser');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const USERS_TABLE = process.env.USERS_TABLE;
+
+// app.use(bodyParser.json({ string: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo, con Express!');
+});
+
+app.post('/users', (req, res) => {
+  const {userId, name} = req.body;
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId, name
+    }
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  dynamoDB.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({
+        error: 'No se ha podido crear el usuario'
+      })
+    } else {
+      res.json({userId, name});
+    }
+  });
+});
+
+app.get('/users', (req, res) => {
+  const params = {
+    TableName: USERS_TABLE,
+  };
+
+  dynamoDB.scan(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({
+        error: 'No se ha podido obtener la lista de usuarios'
+      })
+    } else {
+      const { Items } = result;
+      res.json({
+        success: true,
+        message: 'Usuarios obtenidos correctamente',
+        users: Items,
+      });
+    }
+  });
+});
+
+app.get('/users/:userId', (req, res) => {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId
+    }
+  };
+
+  dynamoDB.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      return res.status(400).json({
+        error: 'No se ha podido acceder al usuario'
+      })
+    }
+
+    if (result.Item) {
+      const { userId, name } = result.Item;
+      res.json({ userId, name });
+    } else {
+      console.log(error);
+      res.status(404).json({
+        error: 'Usuario no encontrado'
+      });
+    }
+  });
+});
+
+module.exports.generic = serverless(app);
